a822242913dd4ba188ec5ba853bffbdf2f9ce096
Alexey Savartsov <asavartsov@gmail.com>
2011-05-12 09:17:49 +0400

    Parser support for Zvooq.ru

--- js/contentscript.js
+++ js/contentscript.js
@@ -110,9 +110,119 @@ AmazonParser.prototype._get_song_album = function() {
     return this._player ? this._player.metadata.albumName : null;
 };
 
+/* Zvooq.ru */
+
+ZvooqParser = function() {
+	// init
+};
+
+/**
+ * Check whether a song loaded into player widget
+ *
+ * @return true if some song is loaded, otherwise false
+ */
+ZvooqParser.prototype._get_has_song = function() {
+    return !($(".playerControls .icon").hasClass("disabled"));
+};
+
+/**
+ * Checks whether song is playing or paused
+ *
+ * @return true if song is playing, false if song is paused
+ */
+ZvooqParser.prototype._get_is_playing = function() {
+    return $(".playerControls .icon").hasClass("icon_top_pause");
+};
+
+/**
+ * Get current song playing position
+ *
+ * @return Playing position in seconds
+ */
+ZvooqParser.prototype._get_song_position = function() {
+    var _time = $(".topPanel_playerPlayback_playered_elapse").text();
+    _time = $.trim(_time).split(':');
+    if(_time.length == 2) {
+        return (parseInt(_time[0], 10) * 60 + parseInt(_time[1], 10));
+    }
+    else {
+        return 0;
+    }
+};
+
+/**
+ * Get current song length
+ *
+ * @return Song length in seconds
+ */
+ZvooqParser.prototype._get_song_time = function() {
+    var _time = $(".topPanel_playerPlayback_playered_leave").text();
+    _time = $.trim(_time.replace(/\//, '')).split(':');
+    if(_time.length == 2) {
+        return (parseInt(_time[0], 10) * 60 + parseInt(_time[1], 10) + this._get_song_position());
+    }
+    else {
+        return 0;
+    }
+};
+
+/**
+ * Get current song title
+ *
+ * @return Song title
+ */
+ZvooqParser.prototype._get_song_title = function() {
+	var song_regex = /^(.*)\ —\ (.*)$/;
+	$(".topPanel_playerPlayback_playered_name").text().match(song_regex);
+	
+    return $.trim(RegExp.$2);
+};
+
+/**
+ * Get current song artist
+ *
+ * @return Song artist
+ */
+ZvooqParser.prototype._get_song_artist = function() {
+	var song_regex = /^(.*)\ —\ (.*)$/;
+	$(".topPanel_playerPlayback_playered_name").text().match(song_regex);
+	
+    return $.trim(RegExp.$1);
+};
+
+
+/**
+ * Get current song album
+ *
+ * @return Song artist
+ */
+ZvooqParser.prototype._get_song_album = function() {
+    return '';
+};
+
+/**
+ * Checks current song artwork
+ *
+ * @return Image URL or default artwork
+ */
+ZvooqParser.prototype._get_song_cover = function() {
+    return null; // Sorry :(
+};
+
+// Port for communicating with background page
 var port = chrome.extension.connect({name: "cloudplayer"});
 
-window.setInterval(function() {
-    port.postMessage(new Player(new AmazonParser()));
-}, 
-10000);	
+// Send song information to the extension every 10 seconds
+
+if(window.location.host === "zvooq.ru") {	
+	window.setInterval(function() {
+        port.postMessage(new Player(new ZvooqParser()));
+    }, 
+    10000);	
+}
+else {	
+	window.setInterval(function() {
+        port.postMessage(new Player(new AmazonParser()));
+    }, 
+    10000);		
+}
