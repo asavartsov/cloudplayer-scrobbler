1149446b0c74d521c2cb6b54bc78fa745cd4fb5a
Andrew Nguyen <atnguyen4@gmail.com>
2013-03-11 16:45:57 -0400

    Added license header

--- js/lastfm_callback.js
+++ js/lastfm_callback.js
@@ -1,3 +1,9 @@
+/**
+ * lastfm_callback.js
+ * LastFM callback script
+ * Copyright (c) 2011 Alexey Savartsov <asavartsov@gmail.com>
+ * Licensed under the MIT license
+ */
 function _url_param(name, url) {
     return unescape((RegExp(name + '=' +
         '(.+?)(&|$)').exec(url) || [,null])[1]);
